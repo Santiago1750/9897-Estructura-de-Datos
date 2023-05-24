ba5d844cc4edb6e8aec748df0becfab1fea3290a
Santiago Arroyo <santiagoarroyovizuete@yahoo.com>
2023-05-24 07:31:59 -0500

    Codigo en Python

---
+++ Colaborador1/Python/HolaMundo.py
@@ -0,0 +1,19 @@
+print("Hola Mundo!")
+2+2 # comentario de media linea
+
+a = 2
+b = 4
+c = a+b
+
+print(c)
+
+num1 = 2
+num2 = 4
+suma = num1 + num2
+print(suma)
+
+_nombre = "santiago"
+
+nombre_completo = "Santiago Arroyo"
+
+nombreCompleto = "Santiago Arroyo"
