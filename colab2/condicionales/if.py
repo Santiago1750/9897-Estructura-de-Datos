8673399b673ae030257f6ff0097cb356bafe9807
Cristopher <cristolasluisa@gmail.com>
2023-05-24 08:22:06 -0500

    condicionales

--- colab2/condicionales/if.py
+++ colab2/condicionales/if.py
@@ -1,9 +1,11 @@
-# igual -->==
-# diferente--> !=
-# mayor o igual que --> >=
-# menor o igual que --> <=
-# mayor que --> >
-# menor que --> >=
+'''
+igual -->==
+diferente--> !=
+mayor o igual que --> >=
+menor o igual que --> <=
+mayor que --> >
+menor que --> >=
+'''
 
 if 2==3:
     print("2 es igual a 2")
@@ -19,3 +21,4 @@ elif 200 <= 100:
     print("9 es menor o igual que 100")        
 else:
     print("No se cumple ninguna condicion")
+
